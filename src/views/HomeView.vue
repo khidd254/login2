22fd37db118289d8771e3ac356af1b15dbf68cc4
khidd <kennedynyambura5981@gmail.com>
2023-06-17 10:19:14 +0300

    init

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,6 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>Welcome to the Home Page</h1>
+    <!-- Add your home page content here -->
   </div>
 </template>
-
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
-</script>
---
+++ src/views/Login.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>Login</h1>
+    <form @submit="login">
+      <label for="username">Username:</label>
+      <input type="text" id="username" v-model="username" required>
+      <label for="password">Password:</label>
+      <input type="password" id="password" v-model="password" required>
+      <button type="submit">Log in</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      username: '',
+      password: ''
+    };
+  },
+  methods: {
+    login(event) {
+      event.preventDefault();
+      this.$router.push('/home');
+    }
+  }
+};
+</script>
